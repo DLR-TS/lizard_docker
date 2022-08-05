a9887dc3b94e119fc5084285917860c339a60bb2
akoerner <alhadrad@gmail.com>
2022-08-05 14:37:48 +0200

    updated exclude paths

--- lizard.sh
+++ lizard.sh
@@ -4,6 +4,19 @@ function echoerr { echo "$@" >&2; exit 1;}
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 set -o pipefail
-lizard --exclud "*/.local/*" --exclude "*/build/*"; \
-lizard --exclud "*/.local/*" --exclude "*/build/*" --output_file=lizard_report.xml -X; \
+lizard \
+        --exclud "*/.local/*" \
+        --exclude "*/build/*" \
+        --exclude "*/cmake/*" \
+        --exclude "*/external/*" \
+        --exclude "*/install/*" \
+        ; \
+lizard \
+        --exclud "*/.local/*" \
+        --exclude "*/build/*" \
+        --exclude "*/cmake/*" \
+        --exclude "*/external/*" \
+        --exclude "*/install/*" \
+        --output_file=lizard_report.xml -X \
+        ; \
 cp lizard_report.xml /tmp/out 
