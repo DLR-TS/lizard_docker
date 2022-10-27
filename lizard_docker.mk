bf53f7c022fdc4a4413839d60849597b082a2e84
Andrew Koerner <andrew.koerner@dlr.de>
2022-10-27 10:54:36 +0200

    WIP: git branch as docker tag

--- lizard_docker.mk
+++ lizard_docker.mk
@@ -1,4 +1,8 @@
 
+ifndef cppcheck_docker.mk
+
+cppcheck_docker.mk:=""
+
 .PHONY: lizard 
 lizard: ## Print out lizard static analysis report.
 	find . -name "**lizard_report.**" -exec rm -rf {} \;
@@ -6,3 +10,5 @@ lizard: ## Print out lizard static analysis report.
     (make lizard CPP_PROJECT_DIRECTORY=$$(realpath ${ROOT_DIR}/${PROJECT}) | \
     tee ${ROOT_DIR}/${PROJECT}/${PROJECT}_lizard_report.log)
 	find . -name "**lizard_report.xml**" -print0 | xargs -0 -I {} mv {} ${PROJECT}/${PROJECT}_lizard_report.xml
+
+endif
